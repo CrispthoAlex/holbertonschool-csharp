bdb44e38b5a4aa3e4e02febc974567583d2facd3
CrispthoAlex <alex877@gmail.com>
2020-12-01 00:48:44 -0500

    Fix tabs

--- 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -4,8 +4,8 @@ class Program
 {
 	static void Main(string[] args)
 	{
-		double percent = .7553;
-		double currency = 98765.4321;
+        double percent = .7553;
+        double currency = 98765.4321;
         Console.WriteLine($"Percent: {percent:0.00%}");
         Console.WriteLine($"Currency: {currency:C}");
     }
