18222bf020dd2f508c377db9cc9d1343802e8a68
CrispthoAlex <alex877@gmail.com>
2020-12-01 23:43:05 -0500

    C# program to print all possible different combinations of two digits.

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sep = ", "; // separator
+            for (int dig1 = 0; dig1 < 10; dig1++)
+            {
+                for (int dig2 = 0; dig2 < 10; dig2++)
+                {
+                    if (dig1 < dig2)
+                    {
+                        Console.Write($"{dig1}{dig2}");
+                        if (!(dig1 == 8 && dig2 == 9))
+                        {
+                            Console.Write(sep);
+                        }
+                    }
+                }
+            }
+            Console.Write("\n");
+        }
+    }
+}
