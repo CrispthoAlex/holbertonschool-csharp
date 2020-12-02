dd72a6383191f6c7aa4591e38f01b9f2566aeaef
CrispthoAlex <alex877@gmail.com>
2020-12-01 21:14:12 -0500

    C# program to print numbers in decimal and hexadecimal

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int num = 0; num < 99; num++)
+            {
+                Console.Write($"{num} = 0x{num.ToString("x")}\n");
+            }
+            
+        }
+    }
+}
