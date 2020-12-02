e103c7f1c33dac5e9a9c5d14c9af4309a996c9ca
CrispthoAlex <alex877@gmail.com>
2020-12-02 00:42:30 -0500

    C# method to draw a diagonal line in the terminal was added

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        int diag, diag2;
+
+        if (length <= 0)
+        {
+            Console.WriteLine("\n");
+        }
+        for (diag = 0; diag < length; diag++)
+        {
+            for (diag2 = 0; diag2 < diag; diag2++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine("\\");
+        }
+        
+    }
+}
