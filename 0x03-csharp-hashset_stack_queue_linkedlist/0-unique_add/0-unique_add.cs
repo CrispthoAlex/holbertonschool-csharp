ff00d52e02bc30b52b71dba375820aa6cc6eea57
CrispthoAlex <alex877@gmail.com>
2020-12-03 18:03:54 -0500

    C#  method that adds all unique integers in a list was added

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+    class List
+    {
+        public static int Sum(List<int> myList)
+        {
+            int sumElem = 0;
+            List<int> sumList = new List<int>();
+            
+            for (int i = 0; i < myList.Count; i++)
+            {
+                if (sumList.Exists(e => e == myList[i]) == false)
+                {
+                    sumList.Add(myList[i]);
+                }
+            }
+            for (int i = 0; i < sumList.Count; i++)
+            {
+                sumElem += sumList[i];
+            }
+            return sumElem;
+        }
+    }
