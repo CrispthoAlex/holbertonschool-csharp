8d54fae0973322dd54e01dfb9983a9f5cd120259
CrispthoAlex <alex877@gmail.com>
2020-12-02 23:05:51 -0500

    C# method to create and print a list of integers of a given size was added

--- 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace _4_print_list
+class List
 {
-    class Program
+    public static List<int> CreatePrint(int size)
     {
-        static void Main(string[] args)
+        if (size < 0)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Size cannot be negative");
+            return null;
         }
+
+        List<int> newList = new List<int>();
+        if (size == 0)
+        {
+            Console.WriteLine("");
+        }
+        else
+        {
+            for (int pos = 0; pos < size; pos++)
+            {
+                newList.Add(pos);
+                Console.Write(newList[pos]);
+                if (pos < size - 1) { Console.Write(" ");}
+            }
+            Console.WriteLine("");
+        }
+        return newList;
     }
 }
