45a9dfe047f4f9dd90d3145317d7adff9b16df35
CrispthoAlex <alex877@gmail.com>
2020-12-03 03:13:35 -0500

    C# program was modified

--- 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -4,15 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        int[,] array = new int[5, 5];
-        for (int row = 0; row < 5; row++)
+        int rows = 5;
+        int cols = 5;
+        int[,] array = new int[rows, cols];
+
+        for (int row = 0; row < rows; row++)
         {
-            for (int col = 0; col < 5; col++)
+            for (int col = 0; col < cols; col++)
             {
-                if (row == 2 && col == 2) { array[2, 2] = 1; }
+                if (row == 2 && col == 2) { array[row, col] = 1; }
                 else { array[row, col] = 0; }
                 Console.Write(array[row, col]);
-                if (col < 4) { Console.Write(" "); }
+                if (col < cols - 1) { Console.Write(" "); }
             }
             Console.WriteLine("");
         }
