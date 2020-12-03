49843ec89d6a1260e8379444f188097d781b6c2a
CrispthoAlex <alex877@gmail.com>
2020-12-03 01:42:23 -0500

    C# program that creates and prints a 5 by 5 two-dimensional array was added

--- 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 
-namespace _14_rectangular_array
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        int[,] array = new int[5, 5];
+        for (int row = 0; row < 5; row++)
         {
-            Console.WriteLine("Hello World!");
+            for (int col = 0; col < 5; col++)
+            {
+                if (row == 2 && col == 2) { array[2, 2] = 1; }
+                else { array[row, col] = 0; }
+                Console.Write(array[row, col]);
+                if (col < 4) { Console.Write(" "); }
+            }
+            Console.WriteLine("");
         }
     }
 }
