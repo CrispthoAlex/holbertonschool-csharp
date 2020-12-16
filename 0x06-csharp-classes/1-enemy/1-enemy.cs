578f790817346d28a5d093889ba873698e7f4c07
CrispthoAlex <alex877@gmail.com>
2020-12-15 20:55:58 -0500

    A public field and public constructor was added

--- 0x06-csharp-classes/1-enemy/1-enemy.cs
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 
-namespace _1_enemy
+namespace Enemies
 {
-    class Program
+    /// <summary>
+    /// Zombie class that define a zombie by:
+    ///  * public field health
+    ///  * health should be an int and have no value
+    ///  * public constructor: public Zombie()
+    ///      * sets the value of health to 0
+    /// </summary>
+    public class Zombie
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// public field, int and have no value.
+        /// </summary>
+        public int health;
+        /// <summary>
+        /// Zombie public constructor to set the value of 
+        /// health field.
+        /// </summary>
+        public Zombie()
         {
-            Console.WriteLine("Hello World!");
+            this.health = 0;
         }
     }
 }
