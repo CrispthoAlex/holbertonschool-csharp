d61566b873776d8146aa3c6ef955ef7661e3c498
CrispthoAlex <alex877@gmail.com>
2020-12-17 20:57:05 -0500

    Test to Add method was added

--- 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -2,11 +2,16 @@ using NUnit.Framework;
 
 namespace MyMath.Tests
 {
+    [TestFixture]
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        [TestCase(0, 3, 3)]
+        [TestCase(-4, 1, -3)]
+        [TestCase(-8, -3, -11)]
+        public void TestAddIntegers(int a, int b, int result)
         {
+            // Testing Add method
+            Assert.AreEqual(Operations.Add(a, b), result);
         }
 
         [Test]
--- 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -2,7 +2,20 @@
 
 namespace MyMath
 {
-    public class Class1
+    /// <summary>
+    /// Operations class define an adding method
+    /// </summary>
+    public class Operations
     {
+        /// <summary>
+        /// Method to add the integers "a" and "b"
+        /// <param name="a">integer</param>
+        /// <param name="b">integer</param>
+        /// <returns>Sum of a and b integers</returns>
+        /// </summary>
+        public static int Add(int a, int b)
+        {
+            return (a + b);
+        }
     }
 }
