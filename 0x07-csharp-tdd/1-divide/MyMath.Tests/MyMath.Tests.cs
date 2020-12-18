e21d58baf9cdbd5c5b36ad3fadb0d8b6f21f0993
CrispthoAlex <alex877@gmail.com>
2020-12-17 23:42:28 -0500

    A method to divide all elements of a matrix was tested

--- 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -2,17 +2,44 @@ using NUnit.Framework;
 
 namespace MyMath.Tests
 {
+    [TestFixture]
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
+        int[,] realMatrix;
 
-        [Test]
-        public void Test1()
+        [SetUp]
+        public void createMatrix()
         {
-            Assert.Pass();
+            realMatrix = new int[,]
+            { 
+                {0, 3, 6},
+                {9, 12, 15},
+                {18, 21, 24}
+            };
+        }
+        
+        [Test]
+        public void TestMatrixNull()
+        {
+            Assert.IsNull(Matrix.Divide(null, 6));
+        }
+        
+        [Test]
+        public void TestMatrixbyZero()
+        {
+            Assert.IsNull(Matrix.Divide(realMatrix, 0));
+        }
+        
+        [Test]
+        public void TestMatrixby3()
+        {
+            int[,] hopeMatrix = new int[,]
+            { 
+                {0, 1, 2},
+                {3, 4, 5},
+                {6, 7, 8}
+            };
+            Assert.AreEqual(Matrix.Divide(realMatrix, 3), hopeMatrix);
         }
     }
 }
--- 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -2,7 +2,42 @@
 
 namespace MyMath
 {
-    public class Class1
+    /// <summary>
+    /// Matrix class define an method to divide each element of a matrix.
+    /// </summary>
+    public class Matrix
     {
+        /// <summary>
+        /// Method to divide each element of matrix by num and, return a new matrix.
+        /// <param name="matrix">matrix</param>
+        /// <param name="num">integer</param>
+        /// <returns>A new matrix of integers</returns>
+        /// </summary>
+        public static int[,] Divide(int[,] matrix, int num)
+        {
+            int[,] newMatrix;
+            try
+            {
+                newMatrix = new int[matrix.GetLength(0), matrix.GetLength(1)];
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {   
+                    for (int col = 0; col < matrix.GetLength(0); col++)
+                    {
+                        Console.WriteLine(matrix.GetLength(1));
+                        newMatrix[row, col] = matrix[row, col] / num;
+                    }
+                } 
+            }
+            catch (NullReferenceException)
+            {
+                return null;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Num cannot be 0");
+                return null;
+            }
+            return newMatrix;
+        }
     }
 }
