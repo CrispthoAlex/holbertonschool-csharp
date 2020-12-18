0c99ad2266c88b065330df5dc89687fe1ad810b9
CrispthoAlex <alex877@gmail.com>
2020-12-18 12:25:05 -0500

    Method to return the index of the first non-repeating character was added

--- 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
@@ -2,17 +2,16 @@ using NUnit.Framework;
 
 namespace Text.Tests
 {
+    [TestFixture]
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        [TestCase("Hello World", 0)]
+        [TestCase("Hello there", 4)]
+        [TestCase("Hello Ol He", -1)]
+        [TestCase("", -1)]
+        public void TestUniqueChar(string s, int check)
         {
-        }
-
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
+            Assert.AreEqual(Str.UniqueChar(s), check);
         }
     }
 }
--- 0x07-csharp-tdd/4-unique/Text/Text.cs
+++ 0x07-csharp-tdd/4-unique/Text/Text.cs
@@ -1,8 +1,33 @@
 ﻿using System;
+using System.Linq;
 
 namespace Text
 {
-    public class Class1
+    /// <summary>
+    /// Str class define an method to check a unique 
+    /// character in a string.
+    /// </summary>
+    public class Str
     {
+        /// <summary>
+        /// Method to return the index of a non-repeating character.
+        /// <param name="s">String to check</param>
+        /// <returns>index of a non-repeating char, else-1</returns>
+        /// </summary>
+        public static int UniqueChar(string s)
+        {
+            s = s.ToLower();
+            foreach (char c in s)
+            {
+                // Using Count method to check if there is a 
+                // non-repeating char.
+                if (s.Count(ch => ch == c) == 1)
+                {
+                    // return the index of non-repeating char
+                    return s.IndexOf(c);
+                }
+            }
+            return -1;
+        }
     }
 }
