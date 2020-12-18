646a61e89767fb001320610bbd8043853c843b05
CrispthoAlex <alex877@gmail.com>
2020-12-18 17:46:57 -0500

    Method to determine how many words are in a string was tested

--- 0x07-csharp-tdd/5-camelcase/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/5-camelcase/Text.Tests/Text.Tests.cs
@@ -2,17 +2,18 @@ using NUnit.Framework;
 
 namespace Text.Tests
 {
+    [TestFixture]
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        [TestCase("hello World, Open Your Eyes", 5)]
+        [TestCase("hello There, 10 Days Left", 4)]
+        [TestCase("techify4 Team Was Created", 3)]
+        [TestCase("1 2 3 4 5", 0)]
+        [TestCase("", 1)]
+        [TestCase(null, 0)]
+        public void TestCamelCase(string s, int check)
         {
-        }
-
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
+            Assert.AreEqual(Str.CamelCase(s), check);
         }
     }
 }
--- 0x07-csharp-tdd/5-camelcase/Text/Text.cs
+++ 0x07-csharp-tdd/5-camelcase/Text/Text.cs
@@ -1,8 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Text
 {
-    public class Class1
+    /// <summary>
+    /// Str class define an method to determine how many words
+    /// are in a string.
+    /// </summary>
+    public class Str
     {
+        /// <summary>
+        /// Method to determine how many words are in a string. Each
+        /// word begins with a capital letter except the first word.
+        /// <param name="s">String to check</param>
+        /// <returns>number of words in s</returns>
+        /// </summary>
+        public static int CamelCase(string s)
+        {
+            int flagChar = 1; // Flag to verify if an word not contains digit
+            int countWord = 0;
+            
+            try
+            {
+                string[] strList = s.ToUpper().Split(' ', '\'');
+
+                foreach (string word in strList)
+                {
+                    flagChar = 1;
+                    foreach (char c in word)
+                    {
+                        if (Char.IsDigit(c))
+                            flagChar = 0;
+                    }
+                    if (flagChar == 1)
+                        countWord++;
+                }    
+            }
+            catch (NullReferenceException)
+            {                
+                return countWord;
+            }
+            return countWord;
+        }
     }
 }
