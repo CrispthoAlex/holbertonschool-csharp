74ec15631702bf7f354253a66db827082cd359a2
CrispthoAlex <alex877@gmail.com>
2020-12-18 01:23:32 -0500

    A method to check if a string is palindrome was tested

--- 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
@@ -2,17 +2,21 @@ using NUnit.Framework;
 
 namespace Text.Tests
 {
+    [TestFixture]
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
-        public void Test1()
+        public void TestEmptyStr()
         {
-            Assert.Pass();
+            Assert.AreEqual(Str.IsPalindrome(""), true);
+        }
+        
+        [TestCase("Tenet", true)]
+        [TestCase("A man, a plan, a canal: Panama", true)]
+        [TestCase("Holbies", false)]
+        public void TestIsPalindrome(string s, bool check)
+        {
+            Assert.AreEqual(Str.IsPalindrome(s), check);
         }
     }
 }
--- 0x07-csharp-tdd/3-palindrome/Text/Text.cs
+++ 0x07-csharp-tdd/3-palindrome/Text/Text.cs
@@ -2,7 +2,29 @@
 
 namespace Text
 {
-    public class Class1
+    /// <summary>
+    /// Str class define an method to check palindrome string.
+    /// </summary>
+    public class Str
     {
+        /// <summary>
+        /// Method to return True if a string is a palindrome or False if it’s not.
+        /// <param name="s">string</param>
+        /// <returns>True or False</returns>
+        /// </summary>
+        public static bool IsPalindrome(string s)
+        {
+            // Apply String lowercase and stay only letters
+            string newString = String.Join("", s.ToLower().Split(' ', ',', '.', ':', ';', '\''));
+            int strLen = newString.Length - 1;
+            
+            foreach (char letter in newString)
+            {
+                if ( letter != newString[strLen])
+                    return false;
+                strLen--;
+            }
+            return true;
+        }
     }
 }
