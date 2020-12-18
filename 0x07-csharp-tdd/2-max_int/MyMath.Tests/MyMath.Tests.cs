ebffc4139691ea1540970f398e5d08abf71d6181
CrispthoAlex <alex877@gmail.com>
2020-12-18 00:27:36 -0500

    A method to return the max integer in a list was tested

--- 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -1,18 +1,46 @@
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace MyMath.Tests
 {
+    [TestFixture]
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        List<int> listNum;
+
+        [Test]
+        public void TestMaxEmptyList()
         {
+            listNum = new List<int>() {  };
+            Assert.AreEqual(Operations.Max(listNum), 0);
         }
 
         [Test]
-        public void Test1()
+        public void TestMaxNullList()
         {
-            Assert.Pass();
+            listNum = null;
+            Assert.AreEqual(Operations.Max(listNum), 0);
+        }
+        
+        [Test]
+        public void TestMaxPositiveNumber()
+        {
+            listNum = new List<int>() {2, 4, 6};
+            Assert.AreEqual(Operations.Max(listNum), 6);
+        }
+
+        [Test]
+        public void TestMaxNegativeNumber()
+        {
+            listNum = new List<int>() {-2, -4, -6};
+            Assert.AreEqual(Operations.Max(listNum), -2);
+        }
+
+        [Test]
+        public void TestMaxNumber()
+        {
+            listNum = new List<int>() {2, -4, -6};
+            Assert.AreEqual(Operations.Max(listNum), 2);
         }
     }
 }
--- 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
+++ 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -1,8 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MyMath
 {
-    public class Class1
+    /// <summary>
+    /// This class defines a method to operate a list.
+    /// </summary>
+    public class Operations
     {
+        /// <summary>
+        /// a method that returns the max integer in a list of integers.
+        /// <param name="nums">list of integers</param>
+        /// <returns>max integer in list</returns>
+        /// </summary>
+        public static int Max(List<int> nums)
+        {
+            int maxNumber = 0;
+            try
+            {
+                maxNumber = nums[0];
+                
+                foreach (int num in nums)
+                {
+                    if (num > maxNumber)
+                        maxNumber = num;
+                }
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                // List is empty
+                return maxNumber;
+            }
+            catch (NullReferenceException)
+            {
+                // List is null
+                return maxNumber;
+            }
+            return maxNumber;
+        }
     }
 }
