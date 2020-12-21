57313dc7e8e6a9586bb21e31bef973d3da6dc4ca
CrispthoAlex <alex877@gmail.com>
2020-12-21 16:55:06 -0500

    A method to print the names of the available properties and methods of an object was added

--- 0x08-csharp-inheritance/3-type_get/3-type_get.cs
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reflection; // Reflection Represents type declarations for class types, interface types, array types, value types, enumeration types, type parameters, generic type definitions, and open or closed constructed generic types.
 
-namespace _3_type_get
+/// <summary>
+/// Obj class define an method to print object information
+/// </summary>
+class Obj
 {
-    class Program
+    /// <summary>
+    /// Method to print the names of the available properties and methods
+    /// of an object.
+    /// <param name="myObj">Object to print self properties and methods</param>
+    /// <returns>void</returns>
+    /// </summary>
+    public static void Print(object myObj)
     {
-        static void Main(string[] args)
+        // Save object information
+        TypeInfo objInfo = myObj.GetType().GetTypeInfo();
+        // Save  in a list properties and methods of an Object
+        IEnumerable<PropertyInfo> propList = objInfo.GetProperties() ;
+        IEnumerable<MethodInfo> meList = objInfo.GetMethods() ;
+
+        
+        Console.WriteLine($"{myObj.GetType().Name} Properties:");
+        foreach (PropertyInfo prop in propList)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(prop.Name);
         }
-    }
+        Console.WriteLine($"{myObj.GetType().Name} Methods:");
+        foreach (MethodInfo me in meList)
+        {
+            Console.WriteLine(me.Name);
+        }
+     }
 }
