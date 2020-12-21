4fb70e8a53b022c68d6a05f37fd8741289315688
CrispthoAlex <alex877@gmail.com>
2020-12-21 13:38:45 -0500

    XML documentation was added

--- 0x08-csharp-inheritance/0-is/0-is.cs
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -1,7 +1,15 @@
 ﻿using System;
 
+/// <summary>
+/// Obj class define an method to check type of object
+/// </summary>
 class Obj
 {
+    /// <summary>
+    /// Method to add the integers "a" and "b"
+    /// <param name="obj">integer</param>
+    /// <returns>If obj is int True, else False</returns>
+    /// </summary>
     public static bool IsOfTypeInt(object obj)
     {
         return obj is int ;
