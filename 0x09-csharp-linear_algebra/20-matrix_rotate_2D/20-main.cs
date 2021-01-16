01153dc86a4017706cf869829d0c76eb76aec594
CrispthoAlex <alex877@gmail.com>
2021-01-15 20:56:23 -0500

    Method to rotate a square 2D matrix by a given angle was added

---
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-main.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Numerics;
+using System.Windows;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("\n==================== TEST 1 ======================\n");
+        double[,] matrix1 = {
+            {1, 2},
+            {3, 4}
+        };
+        Console.WriteLine($"Matrix1:\nColumns = {matrix1.GetLength(0)}\nRows = {matrix1.GetLength(1)}");
+        
+        double angle = -1.57;
+        Console.WriteLine($"Angle = {angle}");
+        
+        printMatrix(MatrixMath.Rotate2D(matrix1, angle));
+        
+        Console.WriteLine("\n==================== TEST 2 ======================\n");
+        double[,] matrix3 = new double[,] {
+            {-11, -5},
+            {6, 16}
+            //{5, 2}
+        };
+
+        Console.WriteLine($"Matrix3:\nColumns = {matrix3.GetLength(0)}\nRows = {matrix3.GetLength(1)}");
+        Console.WriteLine($"Angle = {angle}");
+        printMatrix(MatrixMath.Rotate2D(matrix3, angle));
+    }
+    // Method to print Matrix
+    public static void printMatrix(double [,] matrix)
+    {
+        if (matrix.Length == 1)
+        {
+            Console.WriteLine("The matrices cannot be multiplied");
+            Console.WriteLine(matrix[0,0]);
+        } else
+        {
+                    
+            Console.WriteLine(
+                $"Dimension (Rank) = {matrix.Rank}\nDimension Column = {matrix.GetLength(0)}\nDimension Row = {matrix.GetLength(1)}\nAll Elements = {matrix.Length}"
+                );
+            
+            Console.WriteLine($"Multiply resulting matrix is: \n");
+            for (int col = 0; col < matrix.GetLength(0); col++)
+            {
+                Console.Write("| ");
+                for (int row = 0; row < matrix.GetLength(1); row++)
+                {
+                    Console.Write($"{matrix[col,row]} ");
+                }
+                Console.WriteLine("|");
+            }
+        }
+    }
+}
---
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+    
+/// <summary>
+/// This Class have a method to rotate a matrix.
+/// </summary>
+class MatrixMath
+{
+    /// <summary>
+    /// This Method rotates a square 2D matrix by a given angle in radians.
+    /// </summary>
+    /// <returns>
+    /// The resulting matrix. If the matrix is of an invalid size, return a matrix containing -1.
+    /// </returns>
+    /// <param name="matrix">: Matrix to rotate. </param>
+    /// <param name="angle">: Angle to rotate the matrix. </param>
+    public static double[,] Rotate2D(double[,] matrix, double angle)
+    {
+        double cosinangle = Math.Cos(angle);
+        double sinangle = Math.Sin(angle);
+
+        double[,] rotateMatrix = new double[,] { {cosinangle, sinangle}, {sinangle, cosinangle} };
+        
+        if (matrix is double[,] && rotateMatrix is double[,] &&
+            rotateMatrix.GetLength(1) == matrix.GetLength(0) ) // Columns matrix equal to Rows rotateMatrix.
+        {
+            int colMat1 = matrix.GetLength(0); // Vectors / Columns
+            int rowMat1 = matrix.GetLength(1); // Elements of vector == Rows
+            int rowMat2 = rotateMatrix.GetLength(1); // Elements of vector == Rows
+            int colMat2 = rotateMatrix.GetLength(0); // Vectors / Columns
+
+            double[,] mulMatrix = new double[colMat2, rowMat1];
+
+            // Matrix resulting has the same number of rows as the 1st matrix, and the same number
+            // of columns as the 2nd matrix.
+            for (int col = 0; col < colMat2; col++)
+            {
+                for (int row = 0; row < rowMat1; row++)
+                {
+                    mulMatrix[col, row] = 0;
+                    for (int rxc = 0; rxc <  colMat1; rxc++)
+                    {
+                        // Move col matrix and row rotateMatrix
+                        mulMatrix[col, row] += Math.Round(matrix[rxc, row] * rotateMatrix[col, rxc], 0) ;
+                    }
+                }
+            }
+            return mulMatrix;
+        }
+        else  { return new double[,]{{-1}}; }
+    }
+}
