f4f244e35d455fbc0c34e96e6204e51b68fca3f0
CrispthoAlex <alex877@gmail.com>
2021-01-19 14:38:50 -0500

    Fail element of operation was added

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -30,7 +30,8 @@ class MatrixMath
                 ) -
                 (
                     matrix[0, 0]*matrix[1, 2]*matrix[2, 1] +
-                    matrix[0, 1]*matrix[1, 0]*matrix[2, 2]
+                    matrix[0, 1]*matrix[1, 0]*matrix[2, 2] +
+                    matrix[0, 2]*matrix[1, 1]*matrix[2, 0]
                 ), 2);
         } else { return -1; }
         
