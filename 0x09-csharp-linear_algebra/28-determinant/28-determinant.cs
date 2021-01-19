ad1da6d92b5a3769051b91f10b90bf5ae8cd1905
CrispthoAlex <alex877@gmail.com>
2021-01-19 14:28:06 -0500

    Math.Round() method was re-organized

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -18,21 +18,22 @@ class MatrixMath
         // dimension 2
         if (2 == matrix.GetLength(0) && 2 == matrix.GetLength(1))
         {
-            detMatrix = matrix[0, 0]*matrix[1, 1] - matrix[0, 1]*matrix[1, 0];
+            detMatrix = Math.Round((matrix[0, 0]*matrix[1, 1] - matrix[0, 1]*matrix[1, 0]), 2);
         } // dimension 3
         else if (3 == matrix.GetLength(0) && 3 == matrix.GetLength(1))
         {
-            detMatrix = (
-                            matrix[0, 0]*matrix[1, 1]*matrix[2, 2] + 
-                            matrix[0, 1]*matrix[1, 2]*matrix[2, 0] +
-                            matrix[0, 2]*matrix[1, 0]*matrix[2, 1]
-                        ) -
-                        (
-                           matrix[0, 0]*matrix[1, 2]*matrix[2, 1] +
-                           matrix[0, 1]*matrix[1, 0]*matrix[2, 2]
-                        );
+            detMatrix = Math.Round(
+                (
+                    matrix[0, 0]*matrix[1, 1]*matrix[2, 2] + 
+                    matrix[0, 1]*matrix[1, 2]*matrix[2, 0] +
+                    matrix[0, 2]*matrix[1, 0]*matrix[2, 1]
+                ) -
+                (
+                    matrix[0, 0]*matrix[1, 2]*matrix[2, 1] +
+                    matrix[0, 1]*matrix[1, 0]*matrix[2, 2]
+                ), 2);
         } else { return -1; }
         
-        return Math.Round(detMatrix, 2);
+        return detMatrix;
     }
 }
