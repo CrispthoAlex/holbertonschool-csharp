3af8ccaf6f649be2b0a497a16d5a2607939b4b2c
CrispthoAlex <alex877@gmail.com>
2021-05-05 05:08:02 -0500

    Base abstract class was updated

--- 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -6,7 +6,9 @@ public abstract class Base
     /// String Member of the class
     public string name = null;
 
+    /// <summary>
     /// ToString method overrided
+    /// </summary>
     public override string ToString()
     {
         return name + " is a " + this.GetType();
