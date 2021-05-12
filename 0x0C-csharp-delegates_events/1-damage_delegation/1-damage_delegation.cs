29b02213a950452ecb2fcda86c79461a08b7c1b2
CrispthoAlex <alex877@gmail.com>
2021-05-12 04:48:58 -0500

    delegate CalculateHealth and, TakeDamage and HealDamage methods were added

--- 0x0C-csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
+++ 0x0C-csharp-delegates_events/1-damage_delegation/1-damage_delegation.cs
@@ -1,12 +1,49 @@
 ﻿using System;
 
-namespace _1_damage_delegation
+
+/// <sumary>Player class</sumary>
+class Player
 {
-    class Program
+    // access the Player’s properties directly from the main file.
+    private string name;
+    private float maxHp;
+    private float hp;
+
+    /// <sumary>Constructor </sumary>
+    public Player(string name= "Player", float maxHp= 100f)
     {
-        static void Main(string[] args)
+        this.name = name;
+        if (maxHp > 0)
         {
-            Console.WriteLine("Hello World!");
+            this.maxHp = maxHp;
         }
+        else
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
+        }
+        this.hp = this.maxHp;
+    }
+    /// <sumary>Method to print the Player health </sumary>
+    public void PrintHealth()
+    {
+        Console.WriteLine($"{name} has {hp} / {maxHp} health");
+    }
+
+    // ***** Damage section *****
+    public delegate void  CalculateHealth(float property);
+
+    /// <sumary>Method to print the Player damage </sumary>
+    public void TakeDamage(float damage)
+    {
+        damage = (damage < 0) ? 0 : damage ;
+        Console.WriteLine($"{name} takes {damage} damage!");
+    }
+
+    /// <sumary>Method to print the Player heals </sumary>
+    public void HealDamage(float heal)
+    {
+        heal = (heal < 0) ? 0 : heal ;
+        Console.WriteLine($"{name} heals {heal} HP!");
     }
 }
