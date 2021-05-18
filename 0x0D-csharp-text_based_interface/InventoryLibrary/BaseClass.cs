2ddc40ef41cf08b33a0a981184101cd4485d8b37
CrispthoAlex <alex877@gmail.com>
2021-05-18 03:51:37 -0500

    Base class was updated

--- 0x0D-csharp-text_based_interface/InventoryLibrary/BaseClass.cs
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -5,4 +5,12 @@ class BaseClass
     string id;
     DateTime date_created;
     DateTime date_updated;
+
+    /// <summary> BaseClass constructor </summary>
+    public BaseClass()
+    {
+        id = Guid.NewGuid().ToString();
+        date_created = DateTime.Now;
+        date_updated = date_created;
+    }
 }
